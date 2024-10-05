137379a0264a5f78e2bfffd2d521512ff8108be9
YuvinDediwalage <yuvindeakin@gmail.com>
2024-10-05 18:08:55 +0530

    Delete health-app/src directory

--- health-app/src/main/java/com/gopherindustries/healthapp/App.java
+++
@@ -1,7 +0,0 @@
-package com.gopherindustries.healthapp;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello, Gopher Industries Health App!");
-    }
-}
