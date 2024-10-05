ca77be6a85d203ef2bc06d6d3def7f0b01ae840b
YuvinD <yuvinpereradyk@gmail.com>
2024-10-05 12:34:37 +0530

    Added Maven project for Gopher Health App

---
+++ health-app/src/main/java/com/gopherindustries/healthapp/App.java
@@ -0,0 +1,7 @@
+package com.gopherindustries.healthapp;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Gopher Industries Health App!");
+    }
+}
